d11b989dbab0446822a8003bec097878de7dae5e
ClayGolem99 <MichaelWintherJespersen@gmail.com>
2026-03-06 16:43:18 +0100

    Update and rename SPI.c to Diverse_Filer/SPI.c

--- Diverse_Filer/SPI.c
+++ Diverse_Filer/SPI.c
@@ -7,10 +7,10 @@
 #include "SPI.h" //Inkluder SPI.h filen
 
 void SPI_MasterInit(void) {
-	DDRB |= (1<<DDB2) | (1<<DDB1) | (1<<DDB0); // Sæt MOSI (PB2), SCK (PB1) og SS (PB0) som output
+	DDRB |= (1<<DDB2) | (1<<DDB1) | (1<<DDB0); // SÃ¦t MOSI (PB2), SCK (PB1) og SS (PB0) som output
 	// SPE: Enable SPI
-	// MSTR: Sæt som Master
-	// CPOL: 1 (Clock er høj som default - krav i opgaven)
+	// MSTR: SÃ¦t som Master
+	// CPOL: 1 (Clock er hÃ¸j som default - krav i opgaven)
 	// SPR1: 1 (Divider 64 giver 250kHz ved 16MHz clock)
 	SPCR = (1<<SPE) | (1<<MSTR) | (1<<CPOL) | (1<<SPR1);
 }
@@ -18,16 +18,16 @@ void SPI_MasterInit(void) {
 void SPI_MasterTransmit(char cData) {
 	// Start transmission ved at skrive til dataregisteret
 	SPDR = cData; 
-	// Vent på at transmissionen er færdig (SPIF bitten i SPSR bliver 1)
+	// Vent pÃ¥ at transmissionen er fÃ¦rdig (SPIF bitten i SPSR bliver 1)
 	while(!(SPSR & (1<<SPIF)));
 }
 
 void SPI_SlaveInit(void) {
-	DDRB |= (1<<DDB3);	// Sæt MISO (PB3) som output
-	SPCR = (1<<SPE); // Enable SPI (MSTR er 0 som standard, så den bliver slave)
+	DDRB |= (1<<DDB3);	// SÃ¦t MISO (PB3) som output
+	SPCR = (1<<SPE); // Enable SPI (MSTR er 0 som standard, sÃ¥ den bliver slave)
 }
 
 char SPI_SlaveReceive(void) {
-	while(!(SPSR & (1<<SPIF))); // Vent på at der er modtaget data
+	while(!(SPSR & (1<<SPIF))); // Vent pÃ¥ at der er modtaget data
 	return SPDR;// Returner det modtagne tegn fra dataregisteret
 }
