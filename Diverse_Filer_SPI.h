d19aa75e03ba9212865b12f0794e3e3fe230e8b6
ClayGolem99 <MichaelWintherJespersen@gmail.com>
2026-03-06 16:43:53 +0100

    Update and rename SPI.h to Diverse_Filer_SPI.h

--- Diverse_Filer_SPI.h
+++ Diverse_Filer_SPI.h
@@ -9,9 +9,9 @@
 #ifndef SPI_H_
 #define SPI_H_
 
-	void SPI_MasterInit(void); // Opsætter MEGA2560 som Master	
+	void SPI_MasterInit(void); // OpsÃ¦tter MEGA2560 som Master	
 	void SPI_MasterTransmit(char cData); // Sender en byte data
-	void SPI_SlaveInit(void); // Opsætter MEGA2560 som Slave
+	void SPI_SlaveInit(void); // OpsÃ¦tter MEGA2560 som Slave
 	char SPI_SlaveReceive(void); // Modtager en byte data
 
 #endif /* SPI_H_ */
